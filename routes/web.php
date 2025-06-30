148a2debc1065975b437c7b8ac404a552e64bbd6
Kalok Lau <kaloklau@protonmail.com>
2025-06-30 11:38:55 +0200

    Remove boilerplate code

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('payments/dashboard', [PaymentController::class, 'dashboard'])->name('payments.index');
 Route::post('payments/upload-csv', [PaymentController::class, 'uploadCsv']);
